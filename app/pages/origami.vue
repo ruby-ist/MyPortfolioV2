180b951e4b272750df133f3fcbdd4540301af246
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2025-12-31 16:21:44 +0530

    margin bottom for origami index mobile view

--- app/pages/origami.vue
+++ app/pages/origami.vue
@@ -17,7 +17,7 @@
       </span>
     </h1>
     <NuxtPage v-if="modelName" />
-    <div v-else ref="origamiGridContainer" class="m-0 md:m-40-0">
+    <div v-else ref="origamiGridContainer" class="m-0-0-40 md:m-40-0">
       <FastScrollButtons
         class="fixed z-1 b-69 md:b-54"
         style="right: var(--scroll-button-right-position)"
