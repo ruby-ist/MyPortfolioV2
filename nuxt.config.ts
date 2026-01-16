c520127db2c444af6af0a49faefea3c00102b904
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2026-01-16 11:09:08 +0530

    Update sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -62,10 +62,10 @@ export default defineNuxtConfig({
   },
   sitemap: {
     urls: [
-      { loc: "/", lastmod: "2026-01-15" },
+      { loc: "/", lastmod: "2026-01-16" },
       { loc: "/origami", lastmod: "2026-01-16" },
-      { loc: "/projects", lastmod: "2026-01-15" },
-      { loc: "/blogs", lastmod: "2026-01-15" },
+      { loc: "/projects", lastmod: "2026-01-16" },
+      { loc: "/blogs", lastmod: "2026-01-16" },
       ...origamiLastMod,
       ...blogLastMod,
     ],
